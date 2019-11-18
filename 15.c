434a3067780919dbd26444bd0cf0ece525a1f1c7
bitan1998 <bitanchakraborty7@gmail.com>
2019-11-18 20:57:22 +0530

    Question no 15

---
+++ 15.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+	int a=2,b=0,c;
+    if( !c)
+    {
+    	c=0;
+    	printf("Value of the variable after initialization=%d",c);
+    }
+    else
+    {
+    	printf("Value of the variable is=%d",c );
+    }
+}
